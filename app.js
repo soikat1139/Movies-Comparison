2c178fdff703dac348cb97ddc2d6f56fad764698
Soikat1139 <soikatabd@gmail.com>
2022-12-31 08:56:14 +0600

    Created Utils.js

--- app.js
+++ app.js
@@ -5,33 +5,26 @@ const fetchData=async (searchTerm)=>{
             S:searchTerm
         }
     });
-
-    console.log(response.data)
+    if(response.data.Error){
+        return [];
+    }
+    return response.data.Search;
 };
 
 const input=document.querySelector('input');
 
-const debounce=(func,delay)=>{
-    let timeoutId;
-    return (...args)=>{
-        if(timeoutId){
-            clearTimeout(timeoutId);
-        }
-        timeoutId=setTimeout(()=>{
-            func.apply(null,args);
-        },delay)
-
-    };
-}
-
-
-
-const onInput =event=>{
+const onInput= async event=>{
+   const movies = await fetchData(event.target.value);
    
+   for(let movie of movies){
+   const div=document.createElement('div');
+    div.innerHTML=`     <img src= "${movie.Poster}" />
+    <h1>${movie.Title}</h1>
+     `;
 
-   fetchData(event.target.value)
-    
+    document.querySelector('#target').appendChild(div);
 
+}
 };
 
-input.addEventListener('input',debounce(onInput,500));
+input.addEventListener('input',debounce(onInput,900));
