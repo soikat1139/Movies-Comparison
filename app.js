36924f30f023b53c697771b24c75eb1a2c1370b3
Soikat1139 <soikatabd@gmail.com>
2022-12-31 07:00:20 +0600

    Continue editing

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log('Hi there!');
+const fetchData=async (searchTerm)=>{
+    const response= await axios.get('http://www.omdbapi.com/',{
+        params:{
+            apikey:84998726,
+            S:searchTerm
+        }
+    });
+
+    console.log(response.data)
+};
+
+const input=document.querySelector('input');
+
+const debounce=(func,delay)=>{
+    let timeoutId;
+    return (...args)=>{
+        if(timeoutId){
+            clearTimeout(timeoutId);
+        }
+        timeoutId=setTimeout(()=>{
+            func.apply(null,args);
+        },delay)
+
+    };
+}
+
+
+
+const onInput =event=>{
+   
+
+   fetchData(event.target.value)
+    
+
+};
+
+input.addEventListener('input',debounce(onInput,500));
